37805988388018e0e1cea4ad891ca7c256e2bb01
cal5barton <cal5barton@yahoo.com>
2013-11-20 19:25:01 -0700

    Customer Module Calls

---
+++ App_Code/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+/// <summary>
+/// Summary description for Customer
+/// </summary>
+public class CustomerModule
+{
+    FishbowlServer FBserver = new FishbowlServer();
+
+    public Customer getCustomer(string key, string customerName)
+    {
+        Customer customerObj = new Customer();
+        string response = FBserver.CustomerGet(key, customerName);
+
+        //Read xml response
+        XmlReader reader = XmlReader.Create(response);
+
+        while (reader.Read())
+        {
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                reader.ReadToFollowing("CustomerID");
+                customerObj.CustomerID = reader.ReadElementContentAsInt();
+            }
+        }
+
+        return customerObj;
+    }
+
+    public List<String> getCustomerList(string key)
+    {
+        List<String> allCustomers = new List<string>();
+        string response = FBserver.CustomerNameList(key);
+
+        //Read xml response
+        XmlReader reader = XmlReader.Create(response);
+
+        while (reader.Read())
+        {
+            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "Name"))
+            {
+                allCustomers.Add(reader.Value.ToString());
+            }
+        }
+
+        return allCustomers;
+    }
+}
--- Settings.aspx.cs
+++ Settings.aspx.cs
@@ -10,9 +10,9 @@ public partial class Settings : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         FishbowlServer fbServer = new FishbowlServer();
-        //fbServer.Connect("161.28.118.41", 28192, "admin", "admin", "customerList");
+        fbServer.Connect("161.28.118.41", 28192, "admin", "admin");
 
-        //if (fbServer.serverMessage != "" && fbServer.serverMessage != null)
+        if (fbServer.serverMessage != "" && fbServer.serverMessage != null)
         {
             //Needs Javascript alert
         }
